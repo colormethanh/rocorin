77c9ba5bc52f7d5b347b2c881d2e04ccbb2efda3
colormethanh <thnguyen1847@gmail.com>
2023-05-30 23:45:36 +0900

    set up auto collapse bar

--- rocorin/src/Header.jsx
+++ rocorin/src/Header.jsx
@@ -1,33 +1,10 @@
 import { useState, useRef, useEffect } from 'react'
 import {gsap} from 'gsap'
 import ScrollTrigger from "gsap/ScrollTrigger";
+import useScrollDirection from './hooks/useScrollDirection';
 import './Header.css'
 
 gsap.registerPlugin(ScrollTrigger)
-
-function useScrollDirection() {
-  const [scrollDirection, setScrollDirection] = useState('up')
-
-  useEffect(() => {
-    let lastScrollY = window.pageYOffset
-
-    const updateScrollDirection = () => {
-      const scrollY = window.pageYOffset
-
-      const direction = scrollY > lastScrollY ? 'down' : 'up'
-      if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < 10 )) {
-        setScrollDirection(direction)
-      }
-      lastScrollY = scrollY > 0 ? scrollY : 0
-    }
-    window.addEventListener("scroll", updateScrollDirection)
-    return () => {
-      window.removeEventListener("scroll", updateScrollDirection) // cleanup
-    }
-  }, [scrollDirection])
-
-  return scrollDirection
-}
 
 export default function Header(props){
   
